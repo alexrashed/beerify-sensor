35f4433b4d6dca5ef852f90fef47a0d9a6ac9f46
Alexander Rashed <alexander.rashed@gmail.com>
2016-09-17 21:16:42 +0200

    Initial README, sensor tests and wiring

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Code running on the beerify sensor board (Raspberry Pi 2B + ADC Pi Plus + 3 FSRs
 0) Mount the board and wire the sensors
 1) Download Raspbian Jessie Lite (We don't need any Libreoffice,...)
 2) Copy the image to the SD card (https://www.raspberrypi.org/documentation/installation/installing-images/linux.md)
-3) Install a desktop manager - we want to develop the scripts live on the board (https://www.raspberrypi.org/forums/viewtopic.php?f=66&t=133691)
+3) Install a desktop manager if needed (https://www.raspberrypi.org/forums/viewtopic.php?f=66&t=133691)
 4) Setup I2C on the Rasperry (https://www.abelectronics.co.uk/i2c-raspbian-wheezy/info.aspx)
 5) Install git
 6) Clone this repo and start the script
---
+++ poll_sensors.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from ABE_ADCPi import ADCPi
+from ABE_helpers import ABEHelpers
+import time
+import os
+
+i2c_helper = ABEHelpers()
+bus = i2c_helper.get_smbus()
+adc = ADCPi(bus, 0x68, 0x69, 18)
+
+while (True):
+
+    # clear the console
+    os.system('clear')
+
+    # read from adc channels and print to screen
+    print ("Channel 1: %02f" % adc.read_voltage(1))
+    print ("Channel 2: %02f" % adc.read_voltage(2))
+    print ("Channel 3: %02f" % adc.read_voltage(3))
+    print ("Channel 4: %02f" % adc.read_voltage(4))
+    print ("Channel 5: %02f" % adc.read_voltage(5))
+    print ("Channel 6: %02f" % adc.read_voltage(6))
+    print ("Channel 7: %02f" % adc.read_voltage(7))
+    print ("Channel 8: %02f" % adc.read_voltage(8))
+
+    # wait 0.5 seconds before reading the pins again
+    time.sleep(0.5)
